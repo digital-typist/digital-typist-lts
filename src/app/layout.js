5acf303bc5aac6b449febb995d06b23373ceb701
Digital Typist <digicomclass@gmail.com>
2025-10-14 12:56:59 +0530

    Made site responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,8 +11,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" >
+      <body >
         <Header />
         <main>{children}</main>
         <Footer />
