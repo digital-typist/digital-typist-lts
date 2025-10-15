0b04f078b319850eac89bfcfc47a80dbc9e0194b
Digital Typist <digicomclass@gmail.com>
2025-10-15 11:51:02 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import ContactForm from "./js/ContactForm";
 
 import "./css/hero.css";
 import "./css/home.css";
+import "./css/services.css";
 
 export const metadata = {
   title: "Digital Typist | Thesis Typing & Formatting Services India",
